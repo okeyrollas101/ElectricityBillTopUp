5b647d456fa8f690c960b5bc4e1660dae6485c40
Mubarak <12859241+mcmubby@users.noreply.github.com>
2021-06-10 10:25:46 +0100

    rafactor

--- PortalLibrary/Models/Customer.cs
+++ PortalLibrary/Models/Customer.cs
@@ -5,11 +5,7 @@ namespace PortalLibrary.Models
     public class Customer : BaseUser
     {
         public string MeterNumber { get; set; }
-        public Customer()
-        {
-            // this.Id = "CUS-" + Guid.NewGuid().ToString();
-            // this.MeterNumber = "MN" + Guid.NewGuid().ToString();
-        }
+        public string TarrifName { get; set; }
         
     }
 }
